142d7048533c4933f5026fba5fb2547e6224cdce
Kevin Huang <yellow.khaos@gmail.com>
2018-11-04 02:05:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(event){
   Handlebars.registerPartial(
     'authorPartial',
-    document.getElementById('author-partial-template')
+    document.getElementById('author-partial-template').innerHTML
   )
 })
 
